a60f033b8908b91b34493132bb3fd70c4395fe0d
Sakshi-Saxena <saxenasakshi1105@gmail.com>
2026-05-26 19:30:49 +0530

    first 8 scripts

---
+++ scripts/for_loop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for char in jethalal babitaji dayaben bhide popatlal; do
+	echo "Gokuldham society:  $char"
+done
+
+echo "---"
+
+# glob pattern: loop over files
+
+for files in ./*.sh; do
+	echo "files found: $files"
+
+done
+
+
+
+
+
